366aaa72abe2c8fedc380f2d07a659b6685b1c2b
StarTurtle8 <william.estvanik@gmail.com>
2022-06-02 15:32:10 -0700

    add myscripts

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,6 @@
 function computerPlay() {
     let rng = Math.floor((Math.random() * 3));
-    console.log(rng);
+    // console.log(rng);
     if (rng === 0 ) {
         return "scissors"
     }
@@ -12,31 +12,81 @@ function computerPlay() {
     }
 }
 
-const scoreArea = document.querySelector('scoreArea')
-const playerScore = document.querySelector('.playerScore')
-const cpuScore = document.querySelector('.cpuScore')
-const buttonChoice = document.querySelector('#paperButton');
-buttonChoice.addEventListener('click', () => {
-    game();
-});
+const scoreArea = document.querySelector('scoreArea');
+const fightArea =document.querySelector('.fightArea');
+const winArea =document.querySelector('.winArea');
+const playerScore = document.querySelector('.playerScore');
+const cpuScore = document.querySelector('.cpuScore');
+
+function reload() {
+    reload = location.reload();
+}
+
+
+const rockChoice = document.querySelector('#rockButton');
+rockChoice.addEventListener('click', () => {
+    game('rock', computerPlay());
+})
+const paperChoice = document.querySelector('#paperButton');
+paperChoice.addEventListener('click', () => {
+    game('paper', computerPlay());
+})
+
+const scissorsChoice = document.querySelector('#scissorsButton');
+scissorsChoice.addEventListener('click', () => {
+    game('scissors', computerPlay());
+})
 aiScore = 0;
 humanScore = 0;
 
-function game() {
-    if (computerPlay() === "paper") {
+function game(choice, cpuChoice) {
+    pInput = choice
+    cInput = cpuChoice
+    if ((pInput === "rock" && cInput === "paper") ||
+        (pInput === "paper" && cInput === "scissors") ||
+        (pInput === "scissors" && cInput === "rock")) {
+        console.log(cInput)
         aiScore++
+        fightArea.style.textAlign = "center"
+        fightArea.textContent = "Computer's " + cInput + " beats your " + pInput
         cpuScore.textContent = "Computer Score: " + aiScore
         if (aiScore === 5) {
-            alert('Cpu Wins!')
-        }
-    }  
-    else {
-        humanScore++
-        playerScore.textContent = "Player Score: " + humanScore
-        if (humanScore === 5 ){
-            alert('You Win!')
+            winArea.textContent = "Computer wins! \n"
+            const playAgain = document.createElement("button");
+            playAgain.textContent = "Play again?";
+            playAgain.addEventListener('click', reload, false);
+            winArea.appendChild(playAgain);
+            document.querySelectorAll('.gameChoices button').forEach(elem => {
+                elem.disabled = true; 
+            })
         }
     }
+    else if ((pInput === "rock" && cInput === "scissors") ||
+             (pInput === "paper" && cInput === "rock") ||
+             (pInput === "scissors" && cInput === "paper")) {  
+        console.log(cInput)
+        humanScore++
+        fightArea.style.textAlign = "center"
+        fightArea.textContent = "Your " + pInput + " beats " + "Computer's "+ cInput
+        playerScore.textContent = "Player Score: " + humanScore
+        if (humanScore === 5 ){
+            winArea.textContent = "You win! \n"
+            const playAgain = document.createElement("button");
+            playAgain.textContent = "Play again?";
+            playAgain.addEventListener('click', reload, false);
+            winArea.appendChild(playAgain);
+            document.querySelectorAll('.gameChoices button').forEach(elem => {
+                elem.disabled = true;
+                
+            })
+        }
+    } 
+    else if (cInput === pInput) {
+            console.log(cInput)
+            fightArea.style.textAlign = "center"
+            fightArea.textContent = pInput + " ties with " + cInput
+        }
+    
 }
 
 // console.log(computerPlay())
